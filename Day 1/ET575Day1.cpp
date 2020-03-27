3842d98f6a2a3ea3c03906afd35554bf8758e403
Majesty5 <55115656+Majesty5@users.noreply.github.com>
2020-03-26 21:37:31 -0400

    Add files via upload

---
+++ Day 1/ET575Day1.cpp
@@ -0,0 +1,34 @@
+// Rashida Thomas
+// ET 575 Intro to C ++ Day 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+/*
+
+Comments!
+Should have author's name, date and definition of the change and a definition of the change itself
+Version Date as well
+
+  cout <<"Version PROGRAM EX_1a_basic_program.cpp      01.28.2020"
+*/
+
+/***************************************************************************************
+BLOCKS OF COMMENTS TO STAND OUT TO MAKE CODING EASIER FOR YOURSELF AND OTHER PROGRAMMERS
+
+****************************************************************************************/
+
+  cout << "Hello World, Your Queen is speaking" << endl << endl << endl << endl;
+
+  cout << "Peace I saw unto thee" << endl; // Comments can exist to the right of active code
+
+//Tab escape charater '/t'
+cout <<"\tIndenting using a single tab to escape character." <<endl;
+cout << endl <<endl;
+  return 0;
+}
+
+//Gang gang gang
